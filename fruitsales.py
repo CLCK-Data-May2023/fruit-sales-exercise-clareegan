2615ab7ce4df55d2146ed6cc150a85989fd63072
Clare E <136653618+clareegan@users.noreply.github.com>
2023-09-05 01:19:18 +0000

    final commits!

--- fruitsales.py
+++ fruitsales.py
@@ -1,6 +1,17 @@
 # import independencies
+import pandas as pd
 
 # create/import data
+data = {
+    'Apples': [35, 41],
+    'Bananas': [21, 34]
+}
+
+df = pd.DataFrame(data, index = ['2017 Sales', '2018 Sales'])  
+
+df.to_csv('fruit.csv')
+
+# print(df)
 
 # create data fram from data
 
